92f11ac22ea81b4662e4fa12b329a20688fa4f38
Mageshwaran88 <mageshwaran882002@gmail.com>
2026-02-15 13:55:04 +0530

    fix: force node 18 and railway build

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
+import { webcrypto } from 'crypto';
+(globalThis as any).crypto = webcrypto;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  await app.listen(process.env.PORT || 7000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
